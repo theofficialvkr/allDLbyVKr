be44d24349c5c13309559598bd9e34bfed0c492c
Vijay Kumar <contactvkr@yahoo.com>
2022-04-23 13:48:09 +0530

    update Meta

--- download.php
+++ download.php
@@ -2,12 +2,36 @@
 
 <html>
    <head>
-      <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="robots" content="noindex, nofollow">
-      <meta name="googlebot" content="noindex, nofollow">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <script type="text/javascript" src="/js/bootstrap_002.js"></script>
+     <meta name="title" content="AllDLbyVKr -  Online Download Video, Audio, GIF Photo from YouTube & More">
+      <meta name="description" content="AllDLbyVKr -  Online download videos from YouTube, All in One DL, Download videos from twitter, instagram, facebook, mxtakatak, josh, ipl, ted, tiktok - Get2Mate">
+      <meta name="keywords" content="AllDLbyVKr - online video downloader website,online video downloader app 2022,online video downloader chrome,online video downloader android,online video downloader for pc,online video downloader free,online video downloader for iphone,online video downloader and trimmer,online youtube video downloader app,best online video download app,online video download browser,online video converter downloader free,youtube video downloader cutter online,how to create online video downloader website,online video downloader download youtube dailymotion... videos with 1-click,online video downloader editor,online video downloader embed code,online video downloader for pc windows 10,online video downloader for sony liv,best online video downloader for android,online youtube video downloader for pc windows 10,online video downloader github,online video downloader iphone free,online video downloader idm,online video editor no download,online video downloader reviews,online video download software,online video download site,online video song download,online class video download sinhala,youtube online download video save to gallery,best online video downloader from any site android,best online video downloader from any site,how to online video download,online video downloader with trim,online youtube video downloader website,online video download karne wala app,online youtube video downloader for pc windows 7,online video download youtube,zee5 video downloader online">
+      <meta name="robots" content="index,follow">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <meta name="language" content="en">
+      <meta name="revisit-after" content="2 days">
+      <meta name="author" content="TheOfficialVKr">
+      <meta itemprop="name" content="Get2Mate -AllDLbyVKr - Online Download Video, Audio, GIF Photo from YouTube & More">
+      <meta itemprop="description" content="AllDLbyVKr -  Online download videos from YouTube, All in One DL, Download videos from twitter, instagram, facebook, mxtakatak, josh, ipl, ted, tiktok - Get2Mate">
+      <meta itemprop="image" content="https://github.com/therealvk/allDLbyVKr/raw/main/logo.png">
+      <meta name="twitter:card" content="summary">
+      <meta name="twitter:title" content="Get2Mate -AllDLbyVKr - Online Download Video, Audio, GIF Photo from YouTube & More">
+      <meta name="twitter:description" content="AllDLbyVKr -  Online download videos from YouTube, All in One DL, Download videos from twitter, instagram, facebook, mxtakatak, josh, ipl, ted, tiktok - Get2Mate">
+      <meta name="twitter:site" content="theofficialvkr">
+      <meta name="twitter:creator" content="itsmethevkr">
+      <meta name="twitter:image:src" content="https://github.com/therealvk/allDLbyVKr/raw/main/logo.png">
+      <meta property="og:title" content="AllDLbyVKr -  Online Download Video, Audio, GIF Photo from YouTube & More">
+      <meta property="og:description" content="AllDLbyVKr -  Online download videos from YouTube, All in One DL, Download videos from twitter, instagram, facebook, mxtakatak, josh, ipl, ted, tiktok - Get2Mate">
+      <meta property="og:image" content="https://github.com/therealvk/allDLbyVKr/raw/main/logo.png">
+      <meta property="og:url" content="/">
+      <meta property="og:site_name" content="AllDLbyVKr -  Online Download Video, Audio, GIF Photo from YouTube & More">
+      <meta property="fb:app_id" content="427710135379286">
+      <meta property="og:locale" content="en_US">
+      <meta property="og:type" content="website">
+      <meta name="msapplication-TileColor" content="#ffffff">
+      <meta name="msapplication-TileImage" content="https://github.com/therealvk/allDLbyVKr/raw/main/logo.png">
+      <meta name="theme-color" content="#ffffff">
+      <meta name="viewport" content="width=device-width ,  initial-scale=1 ,  shrink-to-fit=no">
+         <script type="text/javascript" src="/js/bootstrap_002.js"></script>
       <script src="/js/bootstrap.js"></script>
       <script src="/js/jquery-1.js"></script>
       <link rel="stylesheet" href="/css/w3.css">
